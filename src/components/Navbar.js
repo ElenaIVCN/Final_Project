d97142850c313fd3ac6015e30bfcf50e4207d18f
Elena <elena.dumitru89@gmail.com>
2020-10-13 00:18:05 +0300

    temporarry fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,25 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import FavoriteIcon from "@material-ui/icons/Favorite";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
   appBar: {
     [theme.breakpoints.up("sm")]: {
-      width: `calc(100% - 0px)`,
-      marginLeft: 0,
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
     },
   },
   menuButton: {
@@ -21,10 +30,21 @@ const useStyles = makeStyles(theme => ({
       display: "none",
     },
   },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
+
+  const handleDrawerToggle = () => {};
 
   return (
     <AppBar position='fixed' className={classes.appBar}>
@@ -33,6 +53,7 @@ export default function Navbar() {
           color='inherit'
           aria-label='open drawer'
           edge='start'
+          onClick={handleDrawerToggle}
           className={classes.menuButton}
         >
           <MenuIcon />
